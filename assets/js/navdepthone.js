e4b3ea7bd473bc2adc900586e72bc319d6a57961
Austin Dlugosch <austin@dlugosch-family.com>
2023-01-06 21:28:44 -0600

    Implementing universal navigation bar with JavaScript, organizing assets directory

---
+++ assets/js/navdepthone.js
@@ -0,0 +1,13 @@
+var navbar = `     
+          <ul class="nav">
+            <li><a href="../index.html">Home</a></li>
+            <li><a href="../about.html">About</a></li>
+            <li><a href="../portfolio.html">Digital Portfolio</a></li>
+            <li><a href="../CS-II/Essays/AI%20Sentience.html">AI Sentience</a></li>
+            <li><a href="../CS-II/Essays/Decision%20Making.html">Decision Making</a></li>
+            <li><a href="../CS-II/Essays/Fingerprints.html">Fingerprints</a></li>
+            <li><a href="../CS-II/Projects/Optimal%20Sort.html">Optimal Sorting</a></li>
+            <li><a href="../CS-II/Projects/Business%20Plan/index.html">ISP Proposal</a></li>
+            <li><a href="../CS-I/Projects/partyMode/partyMode.html">Projects</a></li>
+          </ul>`;
+document.body.insertAdjacentHTML("afterbegin", navbar);
