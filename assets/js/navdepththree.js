5e2bba283a133a794a71095259b4ad5bebe26b42
Austin Dlugosch <austin@dlugosch-family.com>
2023-01-09 01:32:01 -0600

    adding ISP Journal and first entry

--- assets/js/navdepththree.js
+++ assets/js/navdepththree.js
@@ -7,7 +7,8 @@ var navbar = `
             <li><a href="../../../CS-II/Essays/Decision%20Making.html">Decision Making</a></li>
             <li><a href="../../../CS-II/Essays/Fingerprints.html">Fingerprints</a></li>
             <li><a href="../../../CS-II/Projects/Optimal%20Sort.html">Optimal Sorting</a></li>
-            <li><a href="../../../CS-II/Projects/Business%20Plan/index.html">ISP Proposal</a></li>
+            <li><a href="../../../CS-II/Projects/ISP/index.html">ISP</a></li>
+            <li><a href="../../../CS-II/Projects/ISP/Journal.html">ISP Journal</a></li>
             <li><a href="../../../CS-I/Projects/partyMode/partyMode.html">Projects</a></li>
           </ul>`;
 document.body.insertAdjacentHTML("afterbegin", navbar);
